60ed9f401929235569718d26e7980a2e2e3b50e7
Tim Hanson <timothy.hanson@kablab.com>
2018-08-20 12:34:59 -0700

    added mock directory

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
+import fileUpload from 'express-fileupload';
 import HomeController from './controllers/home'
 import FilesController from './controllers/files'
 
-const ROOT_DIR = '/home/tim';
-
+const ROOT_DIR = process.env.rootDir || __dirname + '/../mock';
 const app = express();
 const home = new HomeController();
 const files = new FilesController(ROOT_DIR);
@@ -13,6 +13,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + "/../views");
 app.set('port', process.env.port || 3000);
 
+app.use(fileUpload());
 app.use(express.static(ROOT_DIR));
 app.use(express.static(__dirname + '/../public', {dotfiles: 'allow'}));
 app.use(morgan('combined'));
