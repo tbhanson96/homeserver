904201a8d6fb7cc6cb4cda86e983fd4f3ad743fe
Tim Hanson <timothy.hanson@kablab.com>
2018-08-27 15:51:27 -0700

    added simple auth

---
+++ src/controllers/auth.ts
@@ -0,0 +1,41 @@
+import auth from 'basic-auth';
+import secureCompare from 'secure-compare';
+import express, { Router } from 'express';
+
+export default class AuthController {
+
+    private username: string;
+    private password: string;
+    public router: Router;
+
+    constructor(username: string, password: string) {
+        this.username = username;
+        this.password = password;
+        this.router = express.Router();
+        this.router.get('*', this.authenticate.bind(this));
+    }
+
+    private authenticate(req, res, next) {
+        if(this.username === '' || this.password === '') {
+            next();
+            return;
+        }
+        const credentials = auth(req);
+
+        if (credentials) {
+            const usernameEqual = secureCompare(this.username, credentials.name);
+            const passwordEqual = secureCompare(this.password, credentials.pass);
+
+            if (usernameEqual && passwordEqual) {
+                next();
+                return;
+            }
+        }
+
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm=""');
+        res.end('Access denied');
+    }
+
+
+}
--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,24 @@ import express from 'express';
 import morgan from 'morgan';
 import HomeController from './controllers/home'
 import FilesController from './controllers/files'
+import AuthController from './controllers/auth';
 
 const ROOT_DIR = process.env.rootDir || __dirname + '/../mock';
+const USERNAME = process.env.username || '';
+const PASSWORD = process.env.password || '';
 const app = express();
 const home = new HomeController();
 const files = new FilesController(ROOT_DIR);
+const auth = new AuthController(USERNAME, PASSWORD);
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + "/../views");
 app.set('port', process.env.port || 3000);
 
+app.use(morgan('combined'));
+app.use(auth.router);
 app.use(express.static(ROOT_DIR));
 app.use(express.static(__dirname + '/../public', {dotfiles: 'allow'}));
-app.use(morgan('combined'));
 app.use('/', home.router);
 app.use('/files', files.router);
 
