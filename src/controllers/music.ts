7bcb4e4234540795ca59a338c251b9c910bb21ad
tbhanson96 <tbh20715@gmail.com>
2018-08-28 19:54:04 -0700

    wip

---
+++ src/controllers/music.ts
@@ -0,0 +1,17 @@
+import util from 'util';
+import downloader from 'youtube-mp3';
+import express, { Router } from 'express';
+
+export default class MusicController {
+    public router: Router;
+
+    public constructor() {
+        this.router = express.Router();
+        this.router.get('/', this.index.bind(this));
+    }
+
+    private index(req, res) {
+        res.render('music/index');
+    }
+    
+};
