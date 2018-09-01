d6485dc11a754c4dc2efb9dd5c9c164a88271229
tbhanson96 <tbh20715@gmail.com>
2018-08-31 17:48:35 -0700

    moved startup to bin/homeserver

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,53 @@
 import express from 'express';
 import morgan from 'morgan';
-import options from 'options-parser';
 import fileUpload from 'express-fileupload';
 import HomeController from './controllers/home'
 import FilesController from './controllers/files'
 import AuthController from './controllers/auth';
 
-const { opt } = options.parse({
-    user: {default: ' '},
-    pass: {default: ' '},
-    rootDir: {default: __dirname + '/../mock'},
-    port: {default: 3000}
-});
-const ROOT_DIR = opt.rootDir;
-const USERNAME = opt.user;
-const PASSWORD = opt.pass;
-const PORT = opt.port;
+class Server {
+    private app: any;
+    private home: HomeController;
+    private files: FilesController;
+    private auth: AuthController;
 
-const app = express();
-const home = new HomeController();
-const files = new FilesController(ROOT_DIR);
-const auth = new AuthController(USERNAME, PASSWORD);
+    private ROOT_DIR: string;
+    private EBOOK_DIR: string;
+    private USER: string;
+    private PASSWORD: string;
+    private PORT: number;
 
-app.set('view engine', 'ejs');
-app.set('view options', { root: __dirname + '/../views'})
-app.set('views', __dirname + "/../views");
-app.set('port', PORT);
+    constructor(opts: any) {
+        this.PORT = opts.port;
+        this.ROOT_DIR = opts.rootDir;
+        this.EBOOK_DIR = opts.ebookDir;
+        this.USER = opts.user;
+        this.PASSWORD = opts.pass;
 
-app.use(morgan('combined'));
-app.use(fileUpload());
-app.use(auth.router);
-app.use(express.static(ROOT_DIR));
-app.use(express.static(__dirname + '/../public', {dotfiles: 'allow'}));
-app.use('/', home.router);
-app.use('/files', files.router);
+        this.app = express();
+        this.home = new HomeController();
+        this.files = new FilesController(opts.rootDir);
+        this.auth = new AuthController(opts.user, opts.pass);
+    }
 
-app.listen(app.get('port'));
+    public configure() {
+        this.app.set('view engine', 'ejs');
+        this.app.set('view options', { root: __dirname + '/../views'})
+        this.app.set('views', __dirname + "/../views");
+        this.app.set('port', this.PORT);
+
+        this.app.use(morgan('combined'));
+        this.app.use(fileUpload());
+        this.app.use(this.auth.router);
+        this.app.use(express.static(this.ROOT_DIR));
+        this.app.use(express.static(__dirname + '/../public', {dotfiles: 'allow'}));
+        this.app.use('/', this.home.router);
+        this.app.use('/files', this.files.router);
+    }
+
+    public start() {
+        this.app.listen(this.PORT);
+    }
+};
+
+module.exports = Server;
